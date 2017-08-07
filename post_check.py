4314e9058a5ef0c7f3448ce771a0d5fb166fa587
Chankster <twmegee@gmail.com>
2017-08-07 14:41:17 -0500

    Move more sub specific stuff to config (continuation of #20)

--- post_check.py
+++ post_check.py
@@ -4,6 +4,7 @@ import sys, os
 from ConfigParser import SafeConfigParser
 import praw
 import re
+import ast
 import sqlite3
 import unicodedata
 from datetime import datetime, timedelta
@@ -21,10 +22,13 @@ app_key = cfg_file.get('reddit', 'app_key')
 app_secret = cfg_file.get('reddit', 'app_secret')
 subreddit = cfg_file.get('reddit', 'subreddit')
 flair_db = cfg_file.get('trade', 'flair_db')
-regex = cfg_file.get('post_check', 'regex')
+posttitle_regex = cfg_file.get('post_check', 'posttitle_regex')
+timestamp_regex = cfg_file.get('post_check', 'timestamp_regex')
+blacklist_regex = cfg_file.get('post_check', 'blacklist_regex')
 rules = cfg_file.get('post_check', 'rules')
 upper_hour = cfg_file.getint('post_check', 'upper_hour')
 lower_min = cfg_file.getint('post_check', 'lower_min')
+flairs = ast.literal_eval(cfg_file.get('post_check', 'flairs'))
 
 # configure logging
 logger = LoggerManager().getLogger(__name__)
@@ -33,7 +37,7 @@ def main():
     while True:
         try:
             try:
-                con = sqlite3.connect(flair_db, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+                con = sqlite3.connect(flair_db, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
                 con.row_factory = sqlite3.Row
             except sqlite3.Error, e:
                 logger.error("Error %s:" % e.args[0])
@@ -55,10 +59,11 @@ def main():
                     if post.id not in already_done:
                         clean_title = unicodedata.normalize('NFKD', post.title).encode('ascii', 'ignore')
                         removedpost = False
+                        checktimestamp = False
+                        noreply = False
+
                         already_done.append(post.id)
-                        matchObj = re.search(regex, post.title)
-                        match2Obj = re.search("(\[selling\])|(\[buying\])", post.title, re.IGNORECASE)
-                        if (not matchObj or match2Obj) and not post.distinguished:
+                        if (not re.search(posttitle_regex, post.title) or re.search(blacklist_regex, post.title, re.IGNORECASE)) and not post.distinguished:
                             if post.author.name != username:
                                 logger.warn('Removed post: ' + clean_title + ' by ' + post.author.name)
                                 if not post.approved_by:
@@ -70,50 +75,28 @@ def main():
                         else:
                             log_msg = ""
                             log_msg_level = ""
-                            buyingMatch = re.search("\[H\].*(cash|paypal|\$|google w|ltc|btc|bitcoin|money).*\[W\]", post.title, re.IGNORECASE)
-                            sellingMatch = re.search("\[W\].*(cash|paypal|\$|google w|ltc|btc|bitcoin|money).*", post.title, re.IGNORECASE)
-                            metaMatch = re.search("\[META\].*", post.title)
-                            icMatch = re.search("\[IC\].*", post.title)
-                            gbMatch = re.search("\[GB\].*", post.title)
-                            artisanMatch = re.search("\[Artisan\].*", post.title)
-                            vendorMatch = re.search("\[Vendor\].*", post.title)
-                            if not post.link_flair_text:
-                                if not post.distinguished:
-                                    if sellingMatch:
-                                        post.link_flair_text = 'Selling'
-                                        post.mod.flair(text='Selling', css_class='selling')
-                                        log_msg = "SELL: " + clean_title
-                                    elif buyingMatch:
-                                        post.link_flair_text = 'Buying'
-                                        post.mod.flair(text='Buying', css_class='buying')
-                                        log_msg = "BUY: " + clean_title
-                                    elif metaMatch:
-                                        post.link_flair_text = 'META'
-                                        post.mod.flair(text='META', css_class='meta')
-                                        log_msg = "META: " + clean_title
-                                    elif icMatch:
-                                        post.mod.flair(text='Interest Check', css_class='interestcheck')
-                                        log_msg = "IC: " + clean_title
-                                    elif gbMatch:
-                                        post.mod.flair(text='Group Buy', css_class='groupbuy')
-                                        log_msg = "GB: " + clean_title
-                                    elif artisanMatch:
-                                        post.mod.flair(text='Artisan', css_class='artisan')
-                                        log_msg = "Artisan: " + clean_title
-                                    elif vendorMatch:
-                                        post.mod.flair(text='Vendor', css_class='vendor')
-                                        log_msg = "Vendor: " + clean_title
-                                    else:
-                                        post.link_flair_text = 'Trading'
-                                        post.mod.flair(text='Trading', css_class='trading')
-                                        log_msg = "TRADE: " + clean_title
-                            else:
-                                log_msg = "OTHER: " + clean_title
+                            if not post.distinguished:
+                                for f in flairs:
+                                    if post.link_flair_text:
+                                        if post.link_flair_text == f['name']:
+                                            log_msg = "*" + f['name'] + ": " + clean_title
+                                            if f['timestamp_check']:
+                                                checktimestamp = True
+                                            if f['no_reply']:
+                                                noreply = True
+                                            break
+                                    elif re.search(f['regex'], post.title):
+                                        post.link_flair_text = f['name']
+                                        post.mod.flair(text=f['name'], css_class=f['class'])
+                                        log_msg = f['name'] + ": " + clean_title
+                                        if f['timestamp_check']:
+                                            checktimestamp = True
+                                        if f['no_reply']:
+                                            noreply = True
+                                        break
 
-                            if post.link_flair_text == 'Selling' or post.link_flair_text == 'Trading':
-                                #logging.info('Checking timestamp')
-                                timestampMatch = re.search('http[s]?:\/\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', post.selftext, re.IGNORECASE)
-                                if not timestampMatch:
+                            if checktimestamp:
+                                if not re.search(timestamp_regex, post.selftext, re.IGNORECASE):
                                     log_msg = 'BAD POST (timestamp) - ' + post.id + ' - ' + clean_title + ' - by: ' + post.author.name
                                     log_msg_level = 'warn'
                                     post.report('Missing timestamp')
@@ -136,7 +119,7 @@ def main():
                                         log_msg = 'BAD POST (24hr) - ' + post.id + ' - ' + clean_title + ' - by: ' + post.author.name
                                         log_msg_level = 'warn'
                                         post.report('24 hour rule')
-                                        post.reply('REMOVED: Posting too frequently.  Please read [wiki](/r/' + subreddit + rules ') for posting time limits.  If you believe this is a mistake, please message the [moderators](http://www.reddit.com/message/compose?to=%2Fr%2F' + subreddit + ').').mod.distinguish()
+                                        post.reply('REMOVED: Posting too frequently.  Please read [wiki](/r/' + subreddit + rules + ') for posting time limits.  If you believe this is a mistake, please message the [moderators](http://www.reddit.com/message/compose?to=%2Fr%2F' + subreddit + ').').mod.distinguish()
                                         post.mod.remove()
                                         removedpost = True
 
@@ -152,7 +135,7 @@ def main():
                                                 botcomment = 1
                                 # otherwise spit out user information
                                 # have to check both flair class and regex match.  (flair class is none if just set)
-                                if botcomment == 0 and (not metaMatch or post.link_flair_text != "Meta"):
+                                if botcomment == 0 and (not noreply):
                                     age = str(datetime.utcfromtimestamp(post.author.created_utc))
                                     if str(post.author_flair_text) == "None":
                                         heatware = "None"
